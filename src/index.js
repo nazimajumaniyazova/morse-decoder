729838ab73355a5961703de08c214a3a127714c4
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2022-08-01 09:54:53 +0500

    feat: first solution added

--- src/index.js
+++ src/index.js
@@ -39,7 +39,28 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-}
+    return expr.split('').map(elem =>{
+      if(elem === ' ') {return '**********'}
+      let tmp =''
+      let morseCode = getKeyByValue(elem);
+      for(let i =0;i<morseCode.length;i++){
+        if(morseCode[i]==='-'){
+          tmp = tmp + 11
+        }else{
+          tmp = tmp + 10
+        }
+      }
+      if(tmp.length<10){
+        while(tmp.length < 10){
+          tmp = 0 + tmp
+        }
+      }
+      return tmp
+    }).join('')
+  }
+  function getKeyByValue(value) {
+    return Object.keys(MORSE_TABLE).find(key => MORSE_TABLE[key] === value);
+  }
 
 module.exports = {
     decode
